0b46fcc835a2879ce2eee7095be8f8785904eb94
Roman Filimonov <romatruelove@yandex.ru>
2022-12-09 11:09:05 +0300

    Final code

---
+++ Pr34/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите любое количество положительных трехзначных чисел через запятую");
+ static void Count()
+        {
+            string s = Console.ReadLine();
+            string[] line = s.Split(',');
+            int[] array = new int[line.Length];
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(line[i]);
+                if (array[i] % 2 == 0)
+                {
+                count++;
+                }
+            }
+            Console.WriteLine($"{count}");
+        }
+        Count();
---
+++ Pr38/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введи число элементов в массиве:");
+void MaxMinNumbers()
+{
+    int size = Convert.ToInt32(Console.ReadLine());
+    double[] array = new double[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().NextDouble()*100;
+    foreach (double el in array)
+        Console.Write($"{el} ");
+        Console.WriteLine();
+
+        double min = array[0];
+        double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+        if (array[i]<min)
+        {
+            min = array[i];
+        }
+        else if(array[i]>max)
+        {
+            max = array[i];
+        }
+    Console.WriteLine($"{max-min}");
+}
+MaxMinNumbers();
